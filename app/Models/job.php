283a563c6121518bd7c9fc72e746a75316d8686f
Omi Mazumder <obokas2@gmail.com>
2024-07-25 19:17:51 +0600

    some updates

--- app/Models/job.php
+++ app/Models/job.php
@@ -9,6 +9,10 @@ class Job extends Model{
     use HasFactory;
     
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary']; 
+    protected $fillable = ['title', 'salary'];
+    
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
 
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,7 +20,6 @@
               <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
               <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
               <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-              <!-- <x-nav-link href="/contact" :active="request()->is('contact')" type="button">Contact</x-nav-link> -->
             </div>
           </div>
         </div>
